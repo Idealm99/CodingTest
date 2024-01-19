96e82e199ace80324cd78ab179be93bc20e300ee
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2024-01-19 13:55:43 +0900

    [level 1] Title: 행렬의 덧셈, Time: 21.56 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,19 +1,13 @@
 
 def solution(arr1, arr2):
-    li=[]
     ad=[]
-    count=0
+    
     for row1, row2 in zip(arr1,arr2):
-        for r1,r2 in zip(row1,row2):
-            ad.append(r1+r2)
-            count+=1
+        
+        ad.append([row1[i]+row2[i] for i in range(len(row1))])
         
 
-        if count == len(arr1[0]):
-            count=0
-            li+=[ad]
-            ad=[]
-    return   li      
+    return   ad      
         
         
     
