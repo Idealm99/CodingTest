93ad71fe2edfa78512505d857cd5b02c46134dbc
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2024-01-22 13:54:56 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,13 @@
+def solution(d, budget):
+    
+    d.sort()
+    count=0
+    for i in d :
+        budget -=i
+        if  budget < 0 :
+            break
+        count+=1
+    
+        
+        
+    return count
