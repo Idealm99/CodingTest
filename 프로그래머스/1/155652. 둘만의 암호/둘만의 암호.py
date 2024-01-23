59699bb569ea57d8ef17e5bb10a8b5f87278c5a2
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2024-01-23 15:27:28 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,20 @@
+def solution(s, skip, index):
+    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for a in range(len(skip)):
+        alpha.remove(skip[a])
+    
+    alpha *=3
+        
+    st=''
+    for i in s:
+        st+=alpha[alpha.index(i)+index]
+    return st
+
+
+        
+        
+    
+
+            
+
