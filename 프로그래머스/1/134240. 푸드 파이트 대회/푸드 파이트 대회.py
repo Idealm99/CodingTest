ee7e4a2d861c2eeffeb2b112886b69f7ca311146
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2024-01-26 09:47:41 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,13 +1,12 @@
 def solution(food):
     st=''
-    re=''
     food.pop(0)
     for idx,food_num in enumerate(food):
         count= food_num // 2
         st += str((idx+1))*count 
-        re += str((idx +1))*count
+        
         
     
     
     
-    return re+'0'+st[::-1]
+    return st+'0'+st[::-1]
