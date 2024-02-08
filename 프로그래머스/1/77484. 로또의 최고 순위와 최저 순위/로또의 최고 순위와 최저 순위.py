92e34a1b3e504b12bc59117d5fa81fa1fb8b8e40
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2024-02-08 10:31:09 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,7 +1,6 @@
 def solution(lottos, win_nums):
     count=0
     zero=0
-    no =0
     for i in lottos :
         if i == 0 :
             zero+=1
@@ -13,8 +12,7 @@ def solution(lottos, win_nums):
     # 최저 순위 count 개수
     li = [zero+count,count]
     re=[]
-    
-    
+
     for i in li :
         if i <=1 :
             re.append(6)
@@ -22,6 +20,15 @@ def solution(lottos, win_nums):
             re.append(7-i)
     return re
         
-    
+#     인덱스로 푼 경우    
+#         rank=[6,6,5,4,3,2,1]
+
+#     cnt_0 = lottos.count(0)
+#     ans = 0
+#     for x in win_nums:
+#         if x in lottos:
+#             ans += 1
+#     return rank[cnt_0 + ans],rank[ans]
+
     
     
