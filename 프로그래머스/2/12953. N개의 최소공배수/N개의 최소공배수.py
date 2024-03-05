ae239a640b71165eceebf16380e8c8ebb9be2c24
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2024-03-05 18:22:51 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.46 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,24 +1,33 @@
-def solution(arr):
-    dict={}
-    for num in arr :
-        i=2
-        li=[]
-        while num >1 :
-            if num % i ==0:
-                num /= i
-                li.append(i)
-                i=2
-            else:
-                i+=1
-        for j in set(li):
-            if j in dict:
-                dict[j]=max(dict[j],li.count(j))
-            else:
-                dict[j]=li.count(j)
-    re=1
-    for i in list(dict.keys()):
-        sum=1
-        for _ in range(dict[i]):
-            sum*=i
-        re*=sum
-    return re
+from fractions import gcd
+
+
+def solution(num):      
+    answer = num[0]
+    for n in num:
+        answer = n * answer / gcd(n, answer)
+
+    return answer
+# def solution(arr):
+#     dict={}
+#     for num in arr :
+#         i=2
+#         li=[]
+#         while num >1 :
+#             if num % i ==0:
+#                 num /= i
+#                 li.append(i)
+#                 i=2
+#             else:
+#                 i+=1
+#         for j in set(li):
+#             if j in dict:
+#                 dict[j]=max(dict[j],li.count(j))
+#             else:
+#                 dict[j]=li.count(j)
+#     re=1
+#     for i in list(dict.keys()):
+#         sum=1
+#         for _ in range(dict[i]):
+#             sum*=i
+#         re*=sum
+#     return re
--- 프로그래머스/2/12953. N개의 최소공배수/README.md
+++ 프로그래머스/2/12953. N개의 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 2] N개의 최소공배수 - 12953 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.04 ms
+메모리: 10.7 MB, 시간: 0.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 05일 18:21:40
+2024년 03월 05일 18:22:50
 
 ### 문제 설명
 
