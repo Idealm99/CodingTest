c7bebb8918512163f869a4e4d6f92f57900ec5dd
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2024-03-07 16:08:28 +0900

    [level 2] Title: 멀리 뛰기, Time: 145.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,22 +1,22 @@
+import math
+
 def solution(n):
-    # 1칸 뛰는 경우의 수
-    one_step = 1
-    # 2칸 뛰는 경우의 수
-    two_step = 2
+    count = 0
+    a = n // 2
+    for i in range(0, a + 1):
+        count += math.comb(n - i , i)
+        print(math.comb(n - i * 2, i), n-2*i,i)
+    return count%1234567
 
-    if n == 1:
-        return one_step
-    elif n == 2:
-        return two_step
+# def jumpCase(num):
+#     a, b = 1, 2
+#     for i in range(2,num):
+#         a, b = b, a+b
+#     return b
 
-    # n이 3 이상인 경우
-    for _ in range(3, n + 1):
-        # 이전에 계산된 경우의 수를 이용하여 현재 경우의 수 계산
-        current_step = (one_step + two_step) % 1234567
-        # 다음 계산을 위해 이전 값 갱신
-        one_step, two_step = two_step, current_step
-
-    return current_step % 1234567
-
-# 테스트
-print(solution(4))  # 출력 결과는 5
+# #아래는 테스트로 출력해 보기 위한 코드입니다.
+# print(jumpCase(4))
+        
+    
+    
+    
