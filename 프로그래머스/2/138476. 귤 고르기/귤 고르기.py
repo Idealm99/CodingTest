bb40a5b97dbbd14f9495ea4c8be576800119994b
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2024-03-09 21:07:08 +0900

    [level 2] Title: 귤 고르기, Time: 18.38 ms, Memory: 17.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,21 @@
+def solution(k, tangerine):
+    dict={}
+    for i in tangerine:
+        if i in dict :
+            dict[i]+=1
+        else:
+            dict[i]=1
+    li=list(dict.values())
+    li.sort(reverse=True)
+    if li[0] >= k:
+        return 1
+    
+    count=0
+    for i in li :
+        k-=i
+        count+=1
+        if k<=0 :
+            return count
+                
+                
+            
