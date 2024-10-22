408d1a579a69f78d71067bf3c44a7d3bdfe3db19
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2024-10-22 13:50:37 +0900

    [level 2] Title: H-Index, Time: 502.66 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,19 @@
+from collections import Counter
+def solution(citations):
+    citations.sort()
+    d= dict(Counter(citations))
+    
+
+    max=0
+    for h in range(citations[-1]):
+        sum=0
+        for i in d.keys():
+            if h<=i:
+                sum+=d[i]
+        if h <=sum :
+            max=h
+            
+    return max   
+            
+            
+        
