1656ed1e53eebe985c550040425e3b61e9f06345
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2024-10-23 13:15:45 +0900

    [level 2] Title: [1차] 캐시, Time: 63.52 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,24 +1,46 @@
+#배울만한 코드 collection 의 deque 를 사용하면 더 빠름
+
 def solution(cacheSize, cities):
-    if cacheSize == 0:
-        return 5 * len(cities)  # 캐시 크기가 0이면 모든 도시가 캐시 미스가 됨
-
-    cache = []
-    total_time = 0
-
-    for city in cities:
-        city = city.lower()  # 각 도시 이름을 소문자로 변환
-
-        if city in cache:
-            # 캐시 히트: 이미 캐시에 존재하는 경우
-            cache.remove(city)  # 기존 항목을 제거하고
-            cache.append(city)  # 최근에 사용된 항목을 뒤로 추가
-            total_time += 1
+    import collections
+    cache = collections.deque(maxlen=cacheSize)
+    time = 0
+    for i in cities:
+        s = i.lower()
+        if s in cache:
+            cache.remove(s)
+            cache.append(s)
+            time += 1
         else:
-            # 캐시 미스: 캐시에 존재하지 않는 경우
-            if len(cache) == cacheSize :
-                
-                cache.pop(0)  # 캐시의 첫 번째 항목(오래된 항목)을 제거
-            cache.append(city)  # 새로운 항목을 추가
-            total_time += 5
+            cache.append(s)
+            time += 5
+    return time
 
-    return total_time
+# 내 코드
+
+# def solution(cacheSize, cities):
+#     if cacheSize == 0:
+#         return 5 * len(cities)  # 캐시 크기가 0이면 모든 도시가 캐시 미스가 됨
+
+#     cache = {}
+#     current_time = 0  # 각 도시의 최근 사용 시간을 기록
+#     total_time = 0
+
+#     for city in cities:
+#         city = city.lower()  # 도시 이름을 소문자로 변환
+
+#         if city in cache:
+#             # 캐시 히트: 이미 캐시에 있는 경우
+#             total_time += 1
+#         else:
+#             # 캐시 미스: 캐시에 없는 경우
+#             if len(cache) == cacheSize:  # 캐시가 가득 찬 경우
+#                 # 가장 오래된 항목(즉, 가장 사용 시간이 오래된 도시)을 제거
+#                 oldest_city = min(cache, key=cache.get)  # 사용 시간이 가장 적은 도시
+#                 del cache[oldest_city]
+#             total_time += 5
+
+#         # 도시를 캐시에 추가하거나 업데이트 (최근 사용 시간 갱신)
+#         cache[city] = current_time
+#         current_time += 1
+
+#     return total_time
