81e1e3385b663b3ce0f6ca135ef80a83730a9ce4
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2025-03-05 22:19:30 +0900

    [level 2] Title: 튜플, Time: 433.39 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -32,3 +32,20 @@ def solution(s):
                 answer.append(j)
     
     return answer
+
+
+
+# def solution(s):
+#     # {{, }}를 제거 후 },{ 으로 나누기
+#     data = s[2:-2].split("},{")
+#     # 길이 별로 오름차순 정렬
+#     data = sorted(data, key=lambda x: len(x))
+#     answer = []
+#     for item in data:
+#         # 각각의 원소로 분류 후
+#         item = list(map(int, item.split(",")))
+#         for value in item:
+#             # 포함되어 있지 않으면 input
+#             if value not in answer:
+#                 answer.append(value)
+#     return answer
