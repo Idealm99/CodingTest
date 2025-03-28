52c80b19b59ec5666fb5006b72f8a209ab1b042f
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2025-03-28 09:40:42 +0900

    [level 3] Title: 정수 삼각형, Time: 86.20 ms, Memory: 18.3 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -10,3 +10,10 @@ def solution(triangle):
                 if sum+triangle[y+1][x+i] > memory[y+1][x+i] :
                     memory[y+1][x+i] = sum+triangle[y+1][x+i]
     return max(memory[-1])
+
+# 이런 식으로 아래에서 위로 가는 방법도 있다
+# def solution(triangle):
+#     for i in range(len(triangle)-2, -1, -1):
+#         for j in range(len(triangle[i])):
+#             triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1])
+#     return triangle[0][0]
