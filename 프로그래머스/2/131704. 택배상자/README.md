28f11a4982f3a074947f277c06400ee1392987f0
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2025-05-21 10:57:26 +0900

    [level 2] Title: 택배상자, Time: 464.26 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/README.md
+++ 프로그래머스/2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704?language=python3#) 
 
 ### 성능 요약
 
-메모리: 92.8 MB, 시간: 503.94 ms
+메모리: 92.9 MB, 시간: 464.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 21일 10:36:16
+2025년 05월 21일 10:57:24
 
 ### 문제 설명
 
--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,24 +1,35 @@
 def solution(order):
-    li = [i for i in range(len(order), 0, -1)]
-    sub = []
-    count = 0
-    idx = 0  # order 인덱스
-
-    while li or sub:
-        if li and li[-1] == order[idx]:
-            li.pop()
+    li=[i for i in range(len(order),0,-1)]
+    
+    sub =[]
+    count=1
+    
+    for i in range(1,order[0]):
+        sub.append(li.pop())
+    
+    li.pop()
+    now =1
+    flag=True
+    
+    while flag and now < len(order):
+        if len(li) != 0 and order[now-1]<order[now] :
+            while li:
+                if li[-1] == order[now]:
+                    li.pop()
+                    break
+                else:
+                    sub.append(li.pop())
+            now += 1
             count += 1
-            idx += 1
-        elif sub and sub[-1] == order[idx]:
-            sub.pop()
-            count += 1
-            idx += 1
-        elif li:
-            sub.append(li.pop())
-        else:
-            break
 
-        if idx >= len(order):
-            break
-
+            # print(li,sub)
+        else :
+            if len(sub) != 0 and sub[-1] == order[now] :
+                now+=1
+                count+=1
+                sub.pop()
+                # print(li,sub)
+            else :
+                flag =False
     return count
+                
