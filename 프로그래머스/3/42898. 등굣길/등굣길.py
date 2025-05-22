363fbf1f3001cc60c873c1192475479bf2f3d383
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2025-05-22 12:24:31 +0900

    [level 3] Title: 등굣길, Time: 1.73 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -14,3 +14,23 @@ def solution(m, n, puddles):
                 if x+1 <=m-1 and grid[y][x+1] != -1:
                     grid[y][x+1]+=grid[y][x]
     return grid[n-1][m-1] % MOD
+
+def solution(m, n, puddles):
+    MOD = 10**9+7
+    # 1) puddle 좌표를 0-based set 으로
+    blocked = {(x-1, y-1) for x, y in puddles}
+
+    # 2) 1차원 DP: 열 방향으로만 관리
+    dp = [0] * m
+    dp[0] = 1  # 출발점
+
+    for y in range(n):
+        for x in range(m):
+            if (x, y) in blocked:
+                dp[x] = 0
+            else:
+                # 왼쪽에서 넘어오는 경우만 더하기
+                if x > 0:
+                    dp[x] = (dp[x] + dp[x-1]) 
+
+    return dp[-1]% MOD
