aa254d970eeef682f28f02e93b75260be86f0970
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2025-08-07 14:38:01 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 120.58 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667#) 
 
 ### 성능 요약
 
-메모리: 37 MB, 시간: 113.01 ms
+메모리: 18.5 MB, 시간: 120.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 07일 14:34:50
+2025년 08월 07일 14:37:59
 
 ### 문제 설명
 
--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,32 +1,40 @@
-
 from collections import deque
+
 def solution(queue1, queue2):
-
-    count=0
-    q1=deque(queue1)
-    q2=deque(queue2)
-    q1s=sum(queue1)
-    q2s=sum(queue2)
-    target= (q1s+q2s)/2
-    print(target,q1s,q2s)
-    while q1 and q2 and 4^(len(queue1)+len(queue2))>count:
-        if q1s > q2s :
-            q2.append(q1.popleft())
-            q2s+=q2[-1]
-            q1s-=q2[-1]
-            count+=1
-        elif q1s < q2s:
-            q1.append(q2.popleft())
-            q2s-=q1[-1]
-            q1s+=q1[-1]
-            count+=1
-
-        else:
-            
+    # 조기 종료 조건들
+    total = sum(queue1) + sum(queue2)
+    if total % 2 != 0:  # 홀수면 절반으로 나눌 수 없음
+        return -1
+    
+    target = total // 2
+    q1s = sum(queue1)
+    
+    if q1s == target:  # 이미 같으면 0 반환
+        return 0
+    
+    # 덱 생성
+    q1 = deque(queue1)
+    q2 = deque(queue2)
+    q2s = total - q1s
+    
+    count = 0
+    max_operations = 3 * (len(queue1) + len(queue2))  # 더 합리적인 상한
+    
+    while q1 and q2 and count < max_operations:
+        if q1s == target:
             return count
-
-    return -1
             
-# 10 5 1
-# 2 2 2
-# 5 1 / 2 2 2 10  . 5 1 2 / 2 2 10 . 5 1 2 2 / 2 10   . 5 1 2 2 2 / 10
+        if q1s > target:
+            # q1에서 q2로 이동
+            val = q1.popleft()
+            q2.append(val)
+            q1s -= val
+            count += 1
+        else:
+            # q2에서 q1로 이동
+            val = q2.popleft()
+            q1.append(val)
+            q1s += val
+            count += 1
+    
+    return -1
